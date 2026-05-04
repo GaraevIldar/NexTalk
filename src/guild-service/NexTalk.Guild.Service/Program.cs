1a896a14eb2432ea3c3640157f17e8b1dc6ae7c0
smth-lya <korsakovdaniltankist@yandex.ru>
2026-05-04 03:44:47 +0300

    feat(services): add /healthz and /readyz health endpoints to all .net services

--- src/guild-service/NexTalk.Guild.Service/Program.cs
+++ src/guild-service/NexTalk.Guild.Service/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(
+        builder.Configuration.GetConnectionString("PostgresConnection")!,
+        tags: ["ready"]);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapHealthChecks("/healthz", new HealthCheckOptions { Predicate = _ => false });
+
+app.MapHealthChecks("/readyz", new HealthCheckOptions
+{
+    Predicate = check => check.Tags.Contains("ready")
+});
 
 app.Run();
--- src/websocket-gateway/NextTalk.Websocket.Gateway/Program.cs
+++ src/websocket-gateway/NextTalk.Websocket.Gateway/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapHealthChecks("/healthz", new HealthCheckOptions { Predicate = _ => false });
+
+app.MapHealthChecks("/readyz", new HealthCheckOptions { Predicate = _ => false });
 
 app.Run();
